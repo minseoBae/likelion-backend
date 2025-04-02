6ad357c7617ad4c39a789751fc2dbd732cabeba9
minseoBae <baejjae8218@gmail.com>
2025-04-02 14:36:11 +0900

    feat(aop-threadlocal-practice) 배민서 작업(완료)

--- src/main/java/com/inspire12/likelionbackend/module/core/threadlocal/service/UserPartPartService.java
+++ src/main/java/com/inspire12/likelionbackend/module/core/threadlocal/service/UserPartPartService.java
@@ -1,15 +1,18 @@
 package com.inspire12.likelionbackend.module.core.threadlocal.service;
 
+import com.inspire12.likelionbackend.module.core.threadlocal.context.UserContextHolder;
+import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class UserPartPartService {
     private static Logger log = LoggerFactory.getLogger(UserPartPartService.class);
     public String useCurrentUsername() {
 //        TODO 함수 시그니처를 수정하지 말고 username 을 찍어보기
-        String username = "FIX ME";
+        String username = UserContextHolder.getUser();
         log.info("username : {}", username);
         return username;
     }
